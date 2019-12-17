e0210bc425c0a2d0824ac337865bae34501e2dfb
Guido Santillan <guidosantillan01@gmail.com>
2019-12-17 10:46:23 +0100

    Add ImageScreen screen and ImageDetail component

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import ImageDetail from '../components/ImageDetail';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <Text>Image Screen</Text>
+      <ImageDetail />
+      <ImageDetail />
+      <ImageDetail />
+      <ImageDetail />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
