4ce84c8dc5e9b05f802cdb0204e3405d711b03a3
Guido Santillan <guidosantillan01@gmail.com>
2019-12-17 10:16:20 +0100

    Touchable Opacity in action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
@@ -11,6 +11,13 @@ const HomeScreen = () => {
         }}
         title="Go to Components Demo"
       />
+      <TouchableOpacity
+        onPress={() => {
+          console.log('List Pressed');
+        }}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
