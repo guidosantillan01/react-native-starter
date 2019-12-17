3e28acdf0fe99c76c5e72e79d65190bb90d1bf42
Guido Santillan <guidosantillan01@gmail.com>
2019-12-17 10:31:40 +0100

    Change TouchableOpacity component to Button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = props => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Guido's React Native App</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
         title="Go to Components Demo"
+        onPress={() => navigation.navigate('Components')}
       />
-      <TouchableOpacity
-        onPress={() => {
-          console.log('List Pressed');
-        }}
-      >
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
     </View>
   );
 };
