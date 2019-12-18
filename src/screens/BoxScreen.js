44b710b5e4393537cb0d6a337c8edc320ecdb5cb
Guido Santillan <guidosantillan01@gmail.com>
2019-12-18 10:41:49 +0100

    Basics of Box Object Model

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,22 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.view}>
+      <Text style={styles.text}>Box Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view: {
+    borderWidth: 3,
+    borderColor: 'black'
+  },
+  text: {
+    borderWidth: 1,
+    borderColor: 'red',
+    margin: 20
+  }
+});
 
 export default BoxScreen;
