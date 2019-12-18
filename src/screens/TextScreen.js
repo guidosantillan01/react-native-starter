8fdc95f5dc72fdf397ff9427cc79caf8b49e797f
Guido Santillan <guidosantillan01@gmail.com>
2019-12-18 10:22:20 +0100

    Add functionality to TextScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 // TextInput has ZERO styles by default
 // iOS capitalize input automatically
 
+// We are ALWAYS going to use value, onChangeText and send a callback function.
+
 const TextScreen = () => {
+  const [name, setName] = useState('');
+
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={newValue => setName(newValue)}
       />
+      <Text>My name is {name}</Text>
     </View>
   );
 };
