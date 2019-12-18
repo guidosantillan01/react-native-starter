08fa75dade0e6416352de3f9d7e8e6cb771cee0f
Guido Santillan <guidosantillan01@gmail.com>
2019-12-18 10:11:24 +0100

    Deactivate autoCapitalize and autoCorrent props

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
-//TextInput has ZERO styles by default
+// TextInput has ZERO styles by default
+// iOS capitalize input automatically
 
 const TextScreen = () => {
   return (
     <View>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
     </View>
   );
 };
